da7752556c38bac54b60461ac653ba20eb708250
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-12 12:04:58 -0800

    simplifying example to have empty init files

--- foocategoricals/__init__.py
+++ foocategoricals/__init__.py
@@ -1,5 +1,1 @@
 __version__ = '0.1.0'
-
-import pandas as pd
-
-from cat.cat import catbind
--- foocategoricals/cat/cat.py
+++ foocategoricals/cat/cat.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def catbind(a, b):
   concatenated = pd.concat([pd.Series(a.astype("string")), pd.Series(b.astype("string"))])
   return pd.Categorical(concatenated)
