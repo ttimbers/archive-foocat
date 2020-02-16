48bb0b714881a17a64dcc3f41c00fea64f38bc94
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-15 16:20:29 -0800

    changing to more updated syntax

--- foocat/cat.py
+++ foocat/cat.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def catbind(a, b):
-  concatenated = pd.concat([pd.Series(a.astype("string")), pd.Series(b.astype("string"))])
+  concatenated = pd.concat([pd.Series(a.astype("str")), pd.Series(b.astype("str"))])
   return pd.Categorical(concatenated)
